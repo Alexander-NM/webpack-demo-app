2c8aaa2083c515b404e04030a77bd42d4a2c7f15
Alexander-NM <alexander.n.makoveev@gmail.com>
2025-02-09 06:56:03 +0300

    #8_Cash Busting and HTML webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path")
 
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main.[hash].js",
         path: path.resolve(__dirname, "dist") 
     },
     module: {
@@ -18,5 +19,8 @@ module.exports = {
             ],
           },
         ],
-      }
+    },
+    plugins: [new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    })],
 }
